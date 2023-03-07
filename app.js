d56612568a7e08f530dbcd3d083da790f08e307a
jose valdez <josezepedavaldez30@gmail.com>
2023-03-06 21:45:48 -0800

    initial commit

--- app.js
+++ app.js
@@ -4,7 +4,35 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Rosie";
+let LastName = "Baggins";
+var age = 51;
 
 // Exercise 2
+LastName = "joe";
+age = 52;
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitve = true;
+
+console.log(`${language} was first release in ${createdYear}.`);
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false
+let selectedSize = "M";
+
+console.log(`${product} in size ${stock} with ${price} in stock.`);
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(`${title} and ${pageCount}.`);
+
